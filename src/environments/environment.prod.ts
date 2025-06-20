e07182d9e0c3a6eace1429d4b3893bfa45ec747d
cristianscript <cristian.torres19@hotmail.com>
2025-06-19 23:05:46 -0500

    fixed production server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://eatlifybackend-cva3cnhzf7c6feaq.canadacentral-01.azurewebsites.net/"
+  apiUrl: "https://eatlifybackend-cva3cnhzf7c6feaq.canadacentral-01.azurewebsites.net"
 };
